f5dfcfd5f8c838aee94309181769f2d46264f9c2
Ruslan <rus5009@yandex.ru>
2025-04-07 06:07:14 +0300

    Фильтрации, роль куратора - начало

---
+++ TyEmuNuzhen/Views/Pages/Curator/ChildrensWork/Monitoring.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace TyEmuNuzhen.Views.Pages.Curator.ChildrensWork
+{
+    public partial class Monitoring : Page
+    {
+        public Monitoring()
+        {
+            InitializeComponent();
+        }
+    }
+}
